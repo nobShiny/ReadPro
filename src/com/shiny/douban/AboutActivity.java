1a3ba4b49d2979a384afba090133603916c8bec9
nobShiny <496749946@qq.com>
2015-04-26 00:34:30 +0800

    优化UI,修复数据位置错误等bug , 发布测试版 伴我读 1.01

--- src/com/shiny/douban/AboutActivity.java
+++ src/com/shiny/douban/AboutActivity.java
@@ -35,7 +35,7 @@ public class AboutActivity extends BaseActivity {
 			
 			@Override
 			public void onClick(View v) {
-				intent = new Intent(AboutActivity.this,MusicMainActivity.class);
+				intent = new Intent(AboutActivity.this,MusicPlayActivity.class);
 				startActivity(intent);
 			}
 		});
